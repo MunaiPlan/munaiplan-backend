a953a57b077e43a800298ef43009e6c67606108f
KadirbekSharau <sharaukadr2001@gmail.com>
2024-10-11 22:37:07 +0100

    bug fix

--- internal/infrastructure/drivers/postgres/setup/indexes.sql
+++ internal/infrastructure/drivers/postgres/setup/indexes.sql
@@ -1,2 +1,3 @@
 CREATE UNIQUE INDEX IF NOT EXISTS idx_organizations_email ON organizations (email) WHERE deleted_at IS NULL;
 CREATE UNIQUE INDEX IF NOT EXISTS idx_users_email ON users (email) WHERE deleted_at IS NULL;
+CREATE UNIQUE INDEX IF NOT EXISTS idx_companies_name ON companies (name) WHERE deleted_at IS NULL;
--- internal/infrastructure/drivers/postgres/setup/setup.sql
+++ internal/infrastructure/drivers/postgres/setup/setup.sql
@@ -1,3 +1,2 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA public;
 -- Enable UUID extension
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
